c14972a8d2874d2a35c370c6a245da79ff44d657
Le Cong Lam <leconglam236@gmail.com>
2024-09-21 15:44:00 +0700

    xóa các phần cũ

--- DATN-TTO/database/seeders/DatabaseSeeder.php
+++ DATN-TTO/database/seeders/DatabaseSeeder.php
@@ -12,31 +12,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\User::factory(5)->create();
-        \App\Models\Route::factory(3)->create();
-        \App\Models\Course::factory(4)->create();
-        \App\Models\Module::factory(3)->create();
-        \App\Models\Chapter::factory(5)->create();
-        \App\Models\Video_lesson::factory(6)->create();
-        \App\Models\Lesson::factory(3)->create();
-        \App\Models\URL_quality::factory(5)->create();
-        \App\Models\URL_sub_title::factory(5)->create();
-        \App\Models\Enrollment::factory(5)->create();
-        \App\Models\Timeline::factory(3)->create();
-        \App\Models\Assignment::factory(5)->create();
-        \App\Models\Attempt::factory(5)->create();
-        \App\Models\Student_answer::factory(7)->create();
-        \App\Models\Wallet::factory(5)->create();
-        \App\Models\Transaction::factory(5)->create();
-        \App\Models\Post_Category::factory(5)->create();
-        \App\Models\Post::factory(5)->create();
-        \App\Models\Comment::factory(5)->create();
-        \App\Models\Conversation::factory(5)->create();
-        \App\Models\Message::factory(10)->create();
-        \App\Models\Messenger_status::factory(5)->create();
-        \App\Models\Group_member::factory(6)->create();
-        \App\Models\Feedback_course::factory(3)->create();
-        \App\Models\Feedback_support::factory(3)->create();
+        
 
     }
 }
--- DATN-TTO/routes/web.php
+++ DATN-TTO/routes/web.php
@@ -1,26 +1,4 @@
 <?php
-
-use App\Http\Controllers\admin\AssignmentController;
-use App\Http\Controllers\admin\AttemptController;
-use App\Http\Controllers\admin\ChapterController;
-use App\Http\Controllers\admin\CommentController;
-use App\Http\Controllers\admin\CourseController;
-use App\Http\Controllers\admin\FeedbackCourseController;
-use App\Http\Controllers\admin\FeedbackSupportController;
-use App\Http\Controllers\admin\LessonController;
-use App\Http\Controllers\admin\MessagesController;
-use App\Http\Controllers\admin\PostCategoriesController;
-use App\Http\Controllers\admin\PostController;
-use App\Http\Controllers\admin\RouteController;
-use App\Http\Controllers\admin\StudentAnswerController;
-use App\Http\Controllers\admin\TimelineController;
-use App\Http\Controllers\admin\TransactionController;
-use App\Http\Controllers\admin\UserController;
-use App\Http\Controllers\admin\VideoLessonController;
-use App\Http\Controllers\admin\WalletsController;
-use App\Models\Lesson;
-use App\Models\Timeline;
-use App\Models\Wallet;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -36,42 +14,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
-Route::prefix('admin')->group(function () {
-    // AssignmentController
-    Route::resource('/Assignment', AssignmentController::class);
-    //AttemptsController
-    Route::resource('/Attempts', AttemptController::class);
-    //ChapterController
-    Route::resource('/Chapter', ChapterController::class);
-    //CommentController
-    Route::resource('/Comment', CommentController::class);
-    //CourseController
-    Route::resource('/Course', CourseController::class);
-    //FeedbackCourseController
-    Route::resource('/Feedback-course', FeedbackCourseController::class);
-    //FeedbackSupportController
-    Route::resource('/Feedback-support', FeedbackSupportController::class);
-    //LessonController
-    Route::resource('/Lesson', LessonController::class);
-    //MessagesController
-    Route::resource('/Messages', MessagesController::class);
-    //PostCategoriesController
-    Route::resource('/PostCategoties', PostCategoriesController::class);
-    //PostController
-    Route::resource('/Post', PostController::class);
-    //RouteController
-    Route::resource('/Route', RouteController::class);
-    //StudentAnswerController
-    Route::resource('/StudentAnswers', StudentAnswerController::class);
-    //TimelineController
-    Route::resource('/Timeline', TimelineController::class);
-    //TransactionController
-    Route::resource('/Transaction', TransactionController::class);
-    //UserController
-    Route::resource('/user', UserController::class);
-    //VideoLesssonController
-    Route::resource('/VideoLesson', VideoLessonController::class);
-    //WalletsController
-    Route::resource('/Wallets', WalletsController::class);
 });
